2b23ad3873952fe389217eddcda7ac12c0df5262
M Fahad Ahmed <160360284+mfahadahmed2020@users.noreply.github.com>
2025-05-31 02:15:30 +0500

    Add files via upload

---
+++ 01_Ai_Agents_First/01_UV_Project_01 & 02/app.py
@@ -0,0 +1,42 @@
+from dotenv import load_dotenv
+import os
+from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel, RunConfig
+
+#.venv\scripts\activate
+
+load_dotenv()
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+# Check if the API key is present; if not, raise an error
+if not gemini_api_key:
+    raise ValueError("GEMINI_API_KEY is not set. Please ensure it is defined in your .env file.")
+
+#Reference: https://ai.google.dev/gemini-api/docs/openai
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=external_client
+)
+
+config = RunConfig(
+    model=model,
+    model_provider=external_client,
+    tracing_disabled=True
+)
+
+agent = Agent(
+    name = "Chief - X2 Translator",
+    instructions = "You Are A HelpFul Translation. Always Translate Urdu to English Sentencess InTo Clear and Simple English to Urdu"
+)
+
+response = Runner.run_sync(
+    agent,
+    input = "My Name is M Fahad Ahmed. I Am A Student of GIAIC./nمیرا نام ایم فہد احمد ہے۔ میں جی آئی اے آئی سی کا طالب علم ہوں۔",
+    run_config = config
+)
+
+print(response)
---
+++ 01_Ai_Agents_First/01_UV_Project_01 & 02/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Well Come Back Panaversity Learn Agentic Ai Chief X- 2!")
+
+
+if __name__ == "__main__":
+    main()
