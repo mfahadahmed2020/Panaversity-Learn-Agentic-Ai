c1ce5b7f6dcb29d999b5bc424935a50c30226a40
M Fahad Ahmed <160360284+mfahadahmed2020@users.noreply.github.com>
2025-09-27 01:53:38 +0500

    Topics For Exams
    Exams Topics

---
+++ Topics For Exams/Weather_As_Tools.py
@@ -0,0 +1,27 @@
+from agents import Agent, Runner, function_tool
+from connection import config
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+weather_api_key = os.getenv("WEATHER_API_KEY")
+
+
+@function_tool
+def get_weather(city: str) -> str:
+    response = requests.get(f"http://api.weatherapi.com/v1/current.json?key={weather_api_key}&q={city}"),
+    data = response.json()
+    return (f"The Current Weather In {city} Is {data['current']['temp_c']}°C With {data['current']['condition']['text']}.")
+
+agent = Agent(
+    name = 'Weather Agent',
+    instructions="You Are A Helpful Assistant. Your Task Is To Help The User With Their Queries.",
+    tools=[get_weather]
+)
+
+result = Runner.run_sync(agent,
+                         "What Is The Current Weather In Karachi Today?",
+                         run_config=config)
+
+print(result.final_output)
